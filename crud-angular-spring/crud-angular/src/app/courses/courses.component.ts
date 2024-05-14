d1412fb482e6d3344c9fefa03716a3c9446bb9b7
Junior Silva <zezinhojun@gmail.com>
2024-05-14 17:19:24 -0300

    Front-end finalizado

--- crud-angular-spring/crud-angular/src/app/courses/courses.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses.component.ts
@@ -1,16 +1,26 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
+import { CategoryPipe } from '../shared/pipes/category.pipe';
 import { CoursesService } from './services/courses.service';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [MatTableModule, MatCardModule, MatToolbarModule, HttpClientModule, MatProgressSpinnerModule],
+  imports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpClientModule,
+    MatProgressSpinnerModule,
+    CategoryPipe,
+    MatIconModule
+  ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
