ebbfbaf89beec550f6ded9dc649fcf6c9564bc2c
Junior Silva <zezinhojun@gmail.com>
2024-05-20 08:17:02 -0300

    removido o lombok e adicionado getter e setters

--- crud-angular-spring/crud-spring/src/main/java/com/jose/crudspring/model/Course.java
+++ crud-angular-spring/crud-spring/src/main/java/com/jose/crudspring/model/Course.java
@@ -21,11 +21,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
-@Data
 @Entity
 
 @SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
@@ -52,6 +52,56 @@ public class Course {
     @Convert(converter = StatusConverter.class)
     private StatusAtvOrIna status = StatusAtvOrIna.ACTIVE;
 
+    @NotNull
+    @NotEmpty
+    @Valid
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
     private List<Lesson> lessons = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public StatusAtvOrIna getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusAtvOrIna status) {
+        this.status = status;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    @Override
+    public String toString() {
+        return "Course [id=" + id + ", name=" + name + ", category=" + category + ", status=" + status + ", lessons="
+                + lessons + "]";
+    }
+
 }
